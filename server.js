eb224a8763f880924db55ddd68e1c0cbc24d5c20
Praveen Bamandla <praveen.bamandla@gmail.com>
2014-12-11 09:32:31 +0000

    express example

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
-// my first file
+
+function TasksClass(){
+  
+  var tasks = [];
+  
+  return {
+    getTasks: function() {
+      return tasks;
+    },
+    addTask: function(name){
+      tasks.push({name:name,completed:false});
+    },
+    finishTask:function(index){
+      tasks[index].completed = true;
+    }
+  };
+  
+}
+
+var tasks = TasksClass();
+
+
+tasks.addTask("xyz");
+tasks.addTask("abc");
+tasks.finishTask(1);
+
+//console.log(tasks.getTasks());
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/tasks', function (req, res) {
+  //res.send('Hello World! 2')
+  res.json({tasks:tasks.getTasks()});
+});
+
+app.listen(process.env.PORT, function () {
+
+  console.log('Server started');
+
+});
