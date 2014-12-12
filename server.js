5bb3175e9b598990c26aabb659c4142ae498ade8
Praveen Bamandla <praveen.bamandla@gmail.com>
2014-12-12 07:13:57 +0000

    changed to read/write tasks from mongodb

--- server.js
+++ server.js
@@ -1,33 +1,87 @@
 
-function TasksClass(){
+function TasksClass(db,ObjectID){
   
-  var tasks = [];
+  //var tasks = [];
   
   return {
-    getTasks: function() {
-      return tasks;
+    getTask: function(id,callback) {
+      db.open(function(){
+			db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.find({_id:new ObjectID(id)}).toArray(function(err, item) {
+					callback(item);
+				});				
+			});
+	  });
+    },
+    getTasks: function(callback) {
+      db.open(function(){
+			db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.find().toArray(function(err, items) {
+					callback(items);
+				});				
+			});
+	  });
     },
     addTask: function(name){
-      tasks.push({name:name,completed:false});
+      //tasks.push({name:name,completed:false});
+		db.open(function(){
+			db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.insert({name:name,completed:false},function(){});
+			});
+		});	
+	},		
+    finishTask:function(id){
+     // tasks[index-1].completed = true;
+		db.open(function(){
+			db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.update({_id:new ObjectID(id)}, {$set:{completed:true}},function(){});
+			});
+		});		 
     },
-    finishTask:function(index){
-      tasks[index].completed = true;
-    }
+	deleteTask:function(id){
+		db.open(function(){
+			db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.remove({_id:new ObjectID(id)},function(){});
+			});
+		});		 
+	}
   };
   
 }
 
-var tasks = TasksClass();
 
 
-tasks.addTask("xyz");
-tasks.addTask("abc");
-tasks.finishTask(1);
 
-//console.log(tasks.getTasks());
+
+var MongoClient = require('mongodb').MongoClient
+ 
+var Server = require('mongodb').Server;
+
+var mongoClient = new MongoClient(new Server('127.0.0.1', 27017));
+
+var db = mongoClient.db("newdb");
+
+var ObjectID = require('mongodb').ObjectID;
+
+
+var tasks = TasksClass(db,ObjectID);
+
+//tasks.addTask("test taskabc");
+//tasks.getTasks(function(items){
+//	console.info(items);
+//});
+//tasks.deleteTask("test taskabc");
+
+
 
 var express = require('express');
 var app = express();
+
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -35,7 +89,36 @@ app.get('/', function (req, res) {
 
 app.get('/tasks', function (req, res) {
   //res.send('Hello World! 2')
-  res.json({tasks:tasks.getTasks()});
+  //res.json({tasks:tasks.getTasks()});
+ // res.send("OK");
+  tasks.getTasks(function(tasks){
+    res.json(tasks);
+  });
+});
+
+app.get('/tasks/:id', function (req, res) {
+  //res.send('Hello World! 2')
+  //res.json({tasks:tasks.getTasks()});
+  tasks.getTask(req.params.id,function(task){
+    res.json(task);
+  });
+ // res.send("OK");
+});
+
+app.post('/tasks', function (req, res) {
+	//console.log("Body", req.body);
+  tasks.addTask(req.body.taskname); 
+  res.send("OK");
+});
+
+app.put('/tasks/:id', function (req, res) {
+  tasks.finishTask(req.params.id);
+  res.send("OK");
+});
+
+app['delete']('/tasks/:id', function (req, res) {
+  tasks.deleteTask(req.params.id);
+  res.send("OK");
 });
 
 app.listen(process.env.PORT, function () {
