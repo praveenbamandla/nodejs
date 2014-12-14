0aa5edd785b194c5f3a83eb671fe621127ab3304
Praveen Bamandla <praveen.bamandla@gmail.com>
2014-12-14 18:04:17 +0000

    db changes

--- server.js
+++ server.js
@@ -4,54 +4,54 @@ function TasksClass(db,ObjectID){
   //var tasks = [];
   
   return {
+  
     getTask: function(id,callback) {
-      db.open(function(){
-			db.collection('tasks',{strict:true}, function(err, collection) {
-				collection.find({_id:new ObjectID(id)}).toArray(function(err, item) {
-					callback(item);
-				});				
-			});
-	  });
+      
+		db.collection('tasks',{strict:true}, function(err, collection) {
+			collection.find({_id:new ObjectID(id)}).toArray(function(err, item) {
+				callback(item);
+			});				
+		});
+	  
     },
-    getTasks: function(callback) {
-      db.open(function(){
-			db.collection('tasks',{strict:true}, function(err, collection) {
-				collection.find().toArray(function(err, items) {
-					callback(items);
-				});				
-			});
-	  });
+	
+    getTasks: function(callback) {      
+			
+		db.collection('tasks',{strict:true}, function(err, collection) {
+			collection.find().toArray(function(err, items) {
+				callback(items);
+			});				
+		});
+			
     },
-    addTask: function(name){
-      //tasks.push({name:name,completed:false});
-		db.open(function(){
-			db.collection('tasks',{strict:true}, function(err, collection) {
-				collection.insert({name:name,completed:false},function(){});
-			});
-		});	
+	
+    addTask: function(taskObj){
+      	
+		db.collection('tasks',{strict:true}, function(err, collection) {
+				collection.insert(taskObj,function(){});
+		});
+		
 	},		
     finishTask:function(id){
-     // tasks[index-1].completed = true;
-		db.open(function(){
-			db.collection('tasks',{strict:true}, function(err, collection) {
+     
+		db.collection('tasks',{strict:true}, function(err, collection) {
 				collection.update({_id:new ObjectID(id)}, {$set:{completed:true}},function(){});
-			});
-		});		 
+		});
+		
     },
 	deleteTask:function(id){
-		db.open(function(){
-			db.collection('tasks',{strict:true}, function(err, collection) {
+		
+		db.collection('tasks',{strict:true}, function(err, collection) {
 				collection.remove({_id:new ObjectID(id)},function(){});
-			});
-		});		 
+		});
+				 
 	}
+	
   };
   
 }
 
-
-
-
+///////////////////////////////////////////////////////////////////
 
 var MongoClient = require('mongodb').MongoClient
  
@@ -63,74 +63,76 @@ var db = mongoClient.db("newdb");
 
 var ObjectID = require('mongodb').ObjectID;
 
+ db.open(function(){
+	
+	var tasks = TasksClass(db,ObjectID);
 
-var tasks = TasksClass(db,ObjectID);
-
-//tasks.addTask("test taskabc");
-//tasks.getTasks(function(items){
-//	console.info(items);
-//});
-//tasks.deleteTask("test taskabc");
-
-
-
-var express = require('express');
-var fs = require('fs');
-var app = express();
-
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
-app.use(express.static('public'));
-
-app.get('/', function (req, res) {
-  /*res.set('Content-Type', 'text/html');
-  fs.readFile('backbone.html', function (err, data) {
-     res.send(data);
-  });*/
-  res.send("Server Home");
-});
+	//tasks.addTask("test taskabc");
+	//tasks.getTasks(function(items){
+	//	console.info(items);
+	//});
+	//tasks.deleteTask("test taskabc");
 
 
-app.get('/tasks', function (req, res) {
-  //res.send('Hello World! 2')
-  //res.json({tasks:tasks.getTasks()});
- // res.send("OK");
-  tasks.getTasks(function(tasks){
-    res.json(tasks);
-  });
-});
 
-app.get('/tasks/:id', function (req, res) {
-  //res.send('Hello World! 2')
-  //res.json({tasks:tasks.getTasks()});
-  tasks.getTask(req.params.id,function(task){
-    res.json(task);
-  });
- // res.send("OK");
-});
+	var express = require('express');
+	var fs = require('fs');
+	var app = express();
 
-app.post('/tasks', function (req, res) {
-	//console.log("Body", req.body);
-  tasks.addTask(req.body.taskname); 
-  res.send("OK");
-});
+	var bodyParser = require('body-parser')
+	app.use( bodyParser.json() );       // to support JSON-encoded bodies
+	app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	  extended: true
+	})); 
 
-app.put('/tasks/:id', function (req, res) {
-  tasks.finishTask(req.params.id);
-  res.send("OK");
-});
+	app.use(express.static('public'));
 
-app['delete']('/tasks/:id', function (req, res) {
-  tasks.deleteTask(req.params.id);
-  res.send("OK");
-});
+	app.get('/', function (req, res) {
+	  /*res.set('Content-Type', 'text/html');
+	  fs.readFile('backbone.html', function (err, data) {
+		 res.send(data);
+	  });*/
+	  res.send("Server Home");
+	});
 
-app.listen(process.env.PORT, function () {
 
-  console.log('Server started');
+	app.get('/tasks', function (req, res) {
+	  //res.send('Hello World! 2')
+	  //res.json({tasks:tasks.getTasks()});
+	 // res.send("OK");
+	  tasks.getTasks(function(tasks){
+		res.json(tasks);
+	  });
+	});
 
+	app.get('/tasks/:id', function (req, res) {
+	  //res.send('Hello World! 2')
+	  //res.json({tasks:tasks.getTasks()});
+	  tasks.getTask(req.params.id,function(task){
+		res.json(task);
+	  });
+	 // res.send("OK");
+	});
+
+	app.post('/tasks', function (req, res) {
+		//console.log("Body", req.body);
+	  tasks.addTask(req.body); 
+	  res.send("OK");
+	});
+
+	app.put('/tasks/:id', function (req, res) {
+	  tasks.finishTask(req.params.id);
+	  res.send("OK");
+	});
+
+	app['delete']('/tasks/:id', function (req, res) {
+	  tasks.deleteTask(req.params.id);
+	  res.send("OK");
+	});
+
+	app.listen(8080, function () {
+
+	  console.log('Server started');
+
+	});
 });
